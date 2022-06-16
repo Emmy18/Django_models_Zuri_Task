396b451da09cb5de231fca25cd0ec7a3a54b2ac3
Emmanuel Iduh <emmiduh93@gmail.com>
2022-06-16 10:29:01 +0100

    Updated author in models.py

--- i4g004570qvp/blog/models.py
+++ i4g004570qvp/blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Posts(models.Model):
     title : models.CharField(max_length=200)
     text : models.TextField
-    author : models.ForeignKey('User', on_delete=models.CASCADE)
+    author : models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE,)
     created_date : models.DateTimeField('date published') 
     published_date : models.DateTimeField('date published') 
