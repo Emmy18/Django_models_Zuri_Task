80fa2df149785570ba6ac09385da7d529efb332c
Emmanuel Iduh <emmiduh93@gmail.com>
2022-06-27 22:52:53 +0100

    Updated models

--- i4g004570qvp/blog/models.py
+++ i4g004570qvp/blog/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Posts(models.Model):
-    title : models.CharField(max_length=200)
-    text : models.TextField
-    author : models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE,)
-    created_date : models.DateTimeField('date published') 
-    published_date : models.DateTimeField('date published')
+    title = models.CharField(max_length=200)
+    text = models.TextField
+    author = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE,)
+    created_date = models.DateTimeField('date published') 
+    published_date = models.DateTimeField('date published')
     def __str__(self):
         return self.title
